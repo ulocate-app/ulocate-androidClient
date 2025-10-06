f88697442b1629ee1cdb19340f058eaac83b4941
Lattie <gracemszhao@gmail.com>
2025-10-06 00:24:58 -0400

    completed mini android app tutorial

--- androidApp/app/src/main/java/com/ulocate_androidclient/MainActivity.kt
+++ androidApp/app/src/main/java/com/ulocate_androidclient/MainActivity.kt
@@ -7,11 +7,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.ulocate_androidclient.ui.theme.UlocateandroidClientTheme
+import androidx.compose.ui.unit.dp
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,9 +24,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             UlocateandroidClientTheme {
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Grace",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -32,16 +38,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface(color = Color.Yellow) {
+        Text(
+            text = "Hello, this is edited by: $name!",
+            modifier = modifier.padding(24.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     UlocateandroidClientTheme {
-        Greeting("Android")
+        Greeting("Grace")
     }
 }
